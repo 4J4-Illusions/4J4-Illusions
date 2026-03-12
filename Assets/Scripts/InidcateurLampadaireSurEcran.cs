a63e146abf401ddff5cab6fac44c697921e2eb01
Firefox13590 <pharlyb@gmail.com>
2026-03-12 11:16:04 -0400

    INSANE PROGRES AVEC LAMPADAIRE
    indicateur UI qui est lock aux dimensions 2d du canvas et reflete pos 3d lampadaire

--- Assets/Scripts/InidcateurLampadaireSurEcran.cs
+++ Assets/Scripts/InidcateurLampadaireSurEcran.cs
@@ -1,18 +1,24 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InidcateurLampadaireSurEcran : MonoBehaviour
 {
     [Header("Affectation inspecteur"), Space]
     public GameObject indicateurUI;
+    public GameObject canvas;
     public Camera cameraJoueur;
 
     [Header("Ajustement inspecteur"), Space]
-    public Vector3 ajustPosIndic = new(0, 0, 0);
+    public Vector3 ajustPosIndic;
+    public float fixDecalageY = 0;
     [Header("Paramétrage des options de debug")]
     public bool debugMode = false;
     public bool debugRay, debugIndicPerm;
 
-    Vector2 posConverti2d;
+    Vector3 posConvertie2d, canvasDimensions;
+    // float au lieu de vector2 parce que l'indicateur est carre
+    float indicUIDimensions;
+    bool estVisible, estBonCoteCam;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -21,23 +27,55 @@ public class InidcateurLampadaireSurEcran : MonoBehaviour
         {
             if (debugIndicPerm) indicateurUI.SetActive(true);
         }
+
+        canvasDimensions = canvas.GetComponent<RectTransform>().rect.size;
+        indicUIDimensions = indicateurUI.GetComponent<RectTransform>().rect.width + indicateurUI.GetComponent<Outline>().effectDistance.x;
+        //Debug.Log(indicUIDimensions);
     }
 
     // Update is called once per frame
     void Update()
     {
-        posConverti2d = Camera.main.WorldToScreenPoint(transform.position + ajustPosIndic);
-        Debug.Log("Position 2D : " + posConverti2d);
+        posConvertie2d = Camera.main.WorldToScreenPoint(transform.position + ajustPosIndic);
+        Debug.Log("Position 2D convertie: " + posConvertie2d);
+        estBonCoteCam = posConvertie2d.z > 0;
+        indicateurUI.SetActive(estBonCoteCam);
+        indicateurUI.GetComponent<RectTransform>().anchoredPosition = KeepInsideBorders(posConvertie2d);
+        //indicateurUI.GetComponent<RectTransform>().anchoredPosition = posConvertie2d;
+        //Debug.Log("Position finale indicateur: " + indicateurUI.GetComponent<RectTransform>().anchoredPosition);
 
         if (debugMode)
         {
             if (debugRay)
             {
-                Ray camRay = Camera.main.ScreenPointToRay(posConverti2d);
+                Ray camRay = Camera.main.ScreenPointToRay(posConvertie2d);
                 Debug.DrawRay(camRay.origin, camRay.direction * 100, Color.red);
             }
         }
+    }
 
-        indicateurUI.GetComponent<RectTransform>().anchoredPosition = posConverti2d;
+
+
+    Vector3 KeepInsideBorders(Vector3 posNonFormattee)
+    {
+        if ((posNonFormattee.x - indicUIDimensions) < 0)
+        {
+            posNonFormattee.x = indicUIDimensions;
+        }
+        else if ((posNonFormattee.x + indicUIDimensions) > canvasDimensions.x)
+        {
+            posNonFormattee.x = canvasDimensions.x - indicUIDimensions;
+        }
+
+        if ((posNonFormattee.y - indicUIDimensions) < 0)
+        {
+            posNonFormattee.y = indicUIDimensions - fixDecalageY;
+        }
+        else if ((posNonFormattee.y + indicUIDimensions) > canvasDimensions.y)
+        {
+            posNonFormattee.y = canvasDimensions.y - (indicUIDimensions + fixDecalageY);
+        }
+
+        return posNonFormattee;
     }
 }
